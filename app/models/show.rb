35104da9577cbdf4115563933176c599da33a88d
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-06 03:01:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,11 +17,14 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum
+    self.sum(:rating)
   end
 
   def self.popular_shows
+    self.where("rating >= ?", 5)
   end
 
   def self.shows_by_alphabetical_order
+    self.order(:name)
   end
 end
