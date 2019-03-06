e28d10958113a82caedf2935f62e1c68878d2478
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-06 02:55:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,24 +1,24 @@
 class Show < ActiveRecord::Base
 
-  def self.highest_rating 
-    
-  end 
+  def self.highest_rating
 
-  def self.most_popular_show 
-  end 
+  end
 
-  def self.lowest_rating 
-  end 
+  def self.most_popular_show
+  end
 
-  def self.least_popular_show 
-  end 
+  def self.lowest_rating
+  end
 
-  def self.ratings_sum 
-  end 
+  def self.least_popular_show
+  end
 
-  def self.popular_shows 
-  end 
+  def self.ratings_sum
+  end
 
-  def self.shows_by_alphabetical_order 
-  end 
-end 
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+  end
+end
