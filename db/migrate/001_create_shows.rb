c982dd5de0d92de623474ed700116f7faa9ebfc2
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-06 02:39:13 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration 
+  def change 
+    create_table do |t| 
+     t.string :name 
+     t.string :network
+     t.string :day 
+     t.integer :rating 
+   end 
+  end 
+end 
